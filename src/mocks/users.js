4c23d6d224e3336cb1bac35c4d87cba2b6a6cdea
rhuaannn <rhuanmarques1223@gmail.com>
2023-02-18 17:17:04 -0300

    first api

---
+++ src/mocks/users.js
@@ -0,0 +1,19 @@
+ module.exports = [
+{
+  id:1,
+  name:'Rhuan',
+},
+{
+  id:2,
+  name:'Marques',
+},
+{
+  id:3,
+  name:'Brenda',
+},
+{
+  id:4,
+  name:'Feitosa',
+},
+];
+  
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+const UserController = require('./controllers/UserController')
+
+module.exports = [
+  {
+    endpoint: '/users',
+    method: 'GET',
+    handler: UserController.listUsers,
+  },
+  {
+    endpoint: '/products',
+    method: 'GET',
+    handler: UserController.listUsers,
+  },
+];
+
